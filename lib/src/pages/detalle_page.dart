c1b3e7fee81b9921ec184b61a7a79ac9a2f9bfc4
Angel Gonzales <angelgonzales@angel.local>
2020-05-28 14:55:44 +0200

    ajutes_detalle

--- lib/src/pages/detalle_page.dart
+++ lib/src/pages/detalle_page.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:resto/src/widgets/detail/detail_widget.dart';
 
 class DetallePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(children: <Widget>[
-        Scaffold(
-          //backgroundColor: Colors.transparent,
-          body: CustomScrollView(
-            slivers: <Widget>[
-              _crearAppbar(),
-              SliverList(
-                  delegate: SliverChildListDelegate([
-                SizedBox(
-                  height: 0.0,
-                ),
-                _descripcion(),
-              ])),
-            ],
-          ),
+    return CustomScrollView(
+      slivers: <Widget>[
+        _crearAppbar(),
+        SliverFixedExtentList(
+          itemExtent: 50,
+          delegate: SliverChildListDelegate([
+            Container(color: Colors.red),
+            Container(color: Colors.green),
+            Container(color: Colors.blue),
+          ]),
         ),
-      ]),
+        DetailWidget()
+      ],
     );
   }
-
   Widget _crearAppbar() {
     return SliverAppBar(
       elevation: 2.0,
@@ -42,69 +36,6 @@ class DetallePage extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
-    );
-  }
-
-  Widget _descripcion() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-      child: Column(children: <Widget>[
-        Row(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text(
-                  'La caña',
-                  style: GoogleFonts.barlowCondensed(
-                    fontSize: 40,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  'Tu bar de birras',
-                  style: GoogleFonts.barlowCondensed(
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  ' Precio medio:  €',
-                  style: GoogleFonts.barlowCondensed(
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                //padding: const EdgeInsets.all(8),
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    color: Colors.amber[600],
-                    child: const Center(child: Text('Entry A')),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[500],
-                    child: const Center(child: Text('Entry B')),
-                  ),
-                  Container(
-                    height: 50,
-                    color: Colors.amber[100],
-                    child: const Center(child: Text('Entry C')),
-                  ),
-                ],
-              ),
-            )
-          ],
-        )
-      ]),
     );
   }
 }
